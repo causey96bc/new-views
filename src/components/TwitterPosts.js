41f722675542239db56a34a0313ab8d489940526
causey96bc <causey904.bc@gmail.com>
2022-01-29 15:02:44 -0500

    deconstruct users and display them

--- src/components/TwitterPosts.js
+++ src/components/TwitterPosts.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SideBar from "./SideBar";
+import { fetchOptions } from "../utils/http";
+
+
+const fetchUsers = async () => {
+    const base_url = `https://random-data-api.com/api/users/random_user?size=20`;
+    const data = await fetch(base_url, fetchOptions());
+    return data.json();
+};
+
+const destructUsers = (user) => {
+    const dUsersNames = user.map((u) => {
+        const { first_name, last_name } = u;
+        // console.log("my user", id, first_name);
+        return first_name + " " + last_name;
+
+    });
+    console.log("duser", dUsersNames);
+    return dUsersNames;
+};
+
 const TwitterPosts = () => {
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        // console.log("setting users");
+        fetchUsers().then((user) => {
+            const displayUsers = destructUsers(user);
+            setUsers(displayUsers);
+            console.log("results", users);
+
+        });
+    }, []);
+
+
     return (
         <>
             <h3>heres twitter</h3>
             <SideBar />
+            {users.map((fName) => (<div>{fName}</div>))}
         </>
     );
 
